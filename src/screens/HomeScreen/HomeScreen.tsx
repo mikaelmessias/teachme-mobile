e760b74947324ca17d7f880e7baa9a04be41cd01
MatheusFerreiraR <matheusramos@alunos.utfpr.edu.br>
2022-06-28 21:35:31 -0300

    fit HomeScreen to small screens

--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -1,6 +1,6 @@
 import {useNavigation} from '@react-navigation/native';
 import React from 'react';
-import {Image, ImageBackground, Text, View} from 'react-native';
+import {Image, ImageBackground, Text, View, ScrollView} from 'react-native';
 import Button from '../../components/Button/Button';
 
 import {styles} from './styles';
@@ -22,26 +22,32 @@ const HomeScreen = () => {
       resizeMode="cover"
       style={styles.container}
     >
-      <Image
-        style={styles.logo}
-        source={require('../../assets/logo-light.png')}
-      />
-      <View style={styles.descriptionGroup}>
-        <Text style={styles.description}>Acesse todos os nossos</Text>
-        <Text style={[styles.description, styles.descriptionHighlight]}>
-          recursos fantásticos
-        </Text>
-        <Text style={styles.description}>em poucos cliques</Text>
-      </View>
-      <View>
-        <Button
-          mode="blue"
-          title="Criar uma conta"
-          onPress={handleSignUpPress}
-          style={styles.signUpButton}
+      <ScrollView style={styles.scrollView}>
+        <Image
+          style={styles.logo}
+          source={require('../../assets/logo-light.png')}
         />
-        <Button title="Entrar" onPress={handleLoginPress} />
-      </View>
+        <View style={styles.descriptionGroup}>
+          <Text style={styles.description}>Acesse todos os nossos</Text>
+          <Text style={[styles.description, styles.descriptionHighlight]}>
+            recursos fantásticos
+          </Text>
+          <Text style={styles.description}>em poucos cliques</Text>
+        </View>
+        <View>
+          <Button
+            mode="blue"
+            title="Criar uma conta"
+            onPress={handleSignUpPress}
+            style={styles.signUpButton}
+          />
+          <Button
+            title="Entrar"
+            onPress={handleLoginPress}
+            style={styles.signInButton}
+          />
+        </View>
+      </ScrollView>
     </ImageBackground>
   );
 };
--- src/screens/HomeScreen/styles.ts
+++ src/screens/HomeScreen/styles.ts
@@ -7,14 +7,15 @@ export const styles = StyleSheet.create({
   },
   scrollView: {
     padding: 38,
-    justifyContent: 'space-between',
   },
   logo: {
     width: 261.24,
     height: 64.75,
     alignSelf: 'center',
   },
-  descriptionGroup: {},
+  descriptionGroup: {
+    marginBottom: 108
+  },
   description: {
     fontSize: 48,
     fontWeight: '600',
@@ -24,18 +25,9 @@ export const styles = StyleSheet.create({
     color: '#C5C5C5',
   },
   signUpButton: {
-    marginBottom: 24,
-    paddingVertical: 8,
-    borderRadius: 50,
-    borderColor: 'white',
-    borderWidth: 1,
+    marginBottom: 24
   },
-  loginButton: {
-    backgroundColor: 'white',
-    padding: 8,
-    marginBottom: 80,
-    borderRadius: 50,
-    borderColor: '#B5B5B5',
-    borderWidth: 1,
-  },
+  signInButton: {
+    marginBottom: 100
+  }
 });
